6b71401869f544acbdd9acf4388dd31a33d280cd
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-05 09:20:59 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,11 +1,12 @@
 import pytest
 import inspect
-from assignment import print_love_python, sum_of_n_numbers, sum_of_digits, count_digits, print_from_five_to
+from assignment import print_love_python, sum_of_n_numbers, count_divisible_by_7, print_cubes, print_from_five_to
 
+# Helper function to check for loop usage
 def check_contains_loop(function):
     source = inspect.getsource(function)
     return 'for' in source or 'while' in source
-    
+
 def test1(capsys):
     print_love_python()
     captured = capsys.readouterr()
@@ -22,24 +23,18 @@ def test2(input, expected):
     assert sum_of_n_numbers(input) == expected
     assert check_contains_loop(sum_of_n_numbers)
 
-@pytest.mark.parametrize("input, expected", [
-    (125, 8),
-    (5, 5),
-    (1234, 10),
-    (0, 0)
-])
-def test3(input, expected):
-    assert sum_of_digits(input) == expected
-    assert check_contains_loop(sum_of_digits)
 
-@pytest.mark.parametrize("input, expected", [
-    (123, 3),
-    (9, 1),
-    (56741, 5),
-])
-def test4(input, expected):
-    assert count_digits(input) == expected
-    assert check_contains_loop(count_digits)
+def test3():
+    assert count_divisible_by_7() == 14
+    assert check_contains_loop(count_divisible_by_7)
+
+def test4(capsys):
+    print_cubes()
+    captured = capsys.readouterr()
+    expected_output = [str(i ** 3) for i in range(1, 11)]
+    assert captured.out.strip().splitlines() == expected_output
+    assert check_contains_loop(print_cubes)
+
 
 @pytest.mark.parametrize("input, expected", [
     (9, [5, 6, 7, 8, 9]),
